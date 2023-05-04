e9dea39d504e78ca8dad2a50180cb5d0614e42aa
Denys Volyntsev <denysvolyntsev@gmail.com>
2023-05-04 18:02:20 +0100

    fix

--- init.lua
+++ init.lua
@@ -59,6 +59,11 @@ return {
       },
     },
   },
+  plugins = {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.catppuccin" }
+    -- ... import any community contributed plugins here
+  },
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
